0056732c4720062517c29cdf13e6a35284c833d9
Haifeng Zeng <zenghaifeng@gmail.com>
2017-04-17 15:51:37 -0400

    runnable for the index controller

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,9 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ImportResource("application-context.xml")
 public class DemoApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
